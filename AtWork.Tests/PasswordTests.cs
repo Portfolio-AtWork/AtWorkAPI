0635cb15553ba7523636a20bcd5445bfc0c208bc
GustavoH1709 <gustavoh1709@hotmail.com>
2025-03-12 22:14:51 -0300

    criado projeto para testes unitarios da api

---
+++ AtWork.Tests/PasswordTests.cs
@@ -0,0 +1,33 @@
+﻿using AtWork.Services.Auth;
+
+namespace AtWork.Tests
+{
+    public class PasswordTests
+    {
+        [Fact(DisplayName = "Given a password, return its hash")]
+        public void GivenAPassword_ReturnItsHash()
+        {
+            const string password = "123";
+            const string expectedHash = "MTIz";
+
+            var passwordHasher = new PasswordHashService();
+
+            string generatedHash = passwordHasher.Hash(password);
+
+            Assert.Equal(expectedHash, generatedHash);
+        }
+
+        [Fact(DisplayName = "Given a hash, verify the password")]
+        public void GivenAHash_VerifyThePassword()
+        {
+            const string hash = "MTIz";
+            const string expectedPassword = "123";
+
+            var passwordHasher = new PasswordHashService();
+
+            bool generatedHash = passwordHasher.VerifyPassword(expectedPassword, hash);
+
+            Assert.True(generatedHash);
+        }
+    }
+}
