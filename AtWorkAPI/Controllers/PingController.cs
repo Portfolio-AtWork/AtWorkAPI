13f45b76f44b1238e03bfff563211513e8fd0807
GustavoH1709 <gustavoh1709@hotmail.com>
2025-04-06 18:50:48 -0300

    criado controller para teste

---
+++ AtWorkAPI/Controllers/PingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtWorkAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping() => "Pong...";
+    }
+}
